6e952b192b12c655e92bdbc34b6f732f34713ab3
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-12-11 19:49:14 +0530

    bet added

--- src/com/bridgelabz/gamblingGame.java
+++ src/com/bridgelabz/gamblingGame.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class gamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Game");
+        int money=100;
+        int betMoney=1;
+        System.out.println("start with a stake of " + money +" every day and bet " + betMoney +" every game.");
     }
 }
